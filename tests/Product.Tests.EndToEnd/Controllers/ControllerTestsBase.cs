faa01d59469c24dfb4f8a5c57f55f1bf390ed17e
zielarka <a.zielinskI@optiguard.pl>
2024-08-12 22:39:36 +0200

    poprawa docker

--- tests/Product.Tests.EndToEnd/Controllers/ControllerTestsBase.cs
+++ tests/Product.Tests.EndToEnd/Controllers/ControllerTestsBase.cs
@@ -1,28 +1,7 @@
-﻿using TaskManager.Api;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
-
-namespace TaskManager.Tests.EndToEnd.Controllers
+﻿namespace TaskManager.Tests.EndToEnd.Controllers
 {
     public abstract class ControllerTestsBase
     {
-        protected readonly TestServer Server;
-        protected readonly HttpClient Client;
 
-        protected ControllerTestsBase()
-        {
-            Server = new TestServer(new WebHostBuilder()
-                          .UseStartup<Startup>());
-            Client = Server.CreateClient();
-        }
-
-        protected static StringContent GetPayload(object data)
-        {
-            var json = JsonConvert.SerializeObject(data);
-            return new StringContent(json, Encoding.UTF8, "application/json");
-        }
     }
 }
